4ccca5084e8564ad846ed772a59357e68a86500f
Joao D. Soares <joao.dacol.soares@gmail.com>
2021-09-01 21:50:09 -0300

    feat: add mineBlock method

--- src/Block.ts
+++ src/Block.ts
@@ -7,6 +7,7 @@ export default class Block {
   data: BlockData;
   previousHash: string;
   hash: string;
+  nonce: number;
 
 
   constructor(index: number, timestamp: Date, data: BlockData, previousHash = '') {
@@ -15,10 +16,21 @@ export default class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() : string {
-    const message = this.index.toString() + this.timestamp + this.previousHash + JSON.stringify(this.data);
+    const message = this.index.toString() + this.timestamp + this.previousHash + JSON.stringify(this.data) + this.nonce;
     return SHA256(message).toString();
+  }
+
+  mineBlock(difficulty : number) : void {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.hash = this.calculateHash();
+      this.nonce++;
+      console.log("Block mined: ", this.hash);
+    }
+
+    console.log("HERE, We found it", this.hash)
   }
 }
